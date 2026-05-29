88ed1e32e21e5a70b50be48692d1a14c95616748
Damir <mail@damirmedia.ru>
2026-05-29 23:54:47 +0500

    feat(ui): добавить сетку фильмов MovieGrid
    Презентационный компонент: карточки-постеры со ссылкой на страницу
    фильма, рейтинг поверх постера

---
+++ src/shared/ui/movie-grid.tsx
@@ -0,0 +1,37 @@
+import { FilmDetail } from "@/shared/types/api.types";
+import Image from "next/image";
+import RatingBadge from "@/shared/ui/rating-badge";
+import { Card } from "@/shared/ui/card";
+import Link from "next/link";
+
+function MovieGrid({ films }: { films: FilmDetail[] }) {
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-4">
+      {films.map((film: FilmDetail) => {
+        return (
+          <Link key={film.kinopoiskId} href={`/movies/${film.kinopoiskId}`}>
+            <Card className="relative w-full pt-0 overflow-hidden transition hover:scale-103">
+              <div className="relative aspect-2/3 w-full overflow-hidden">
+                <Image
+                  src={film.posterUrl}
+                  alt={film.nameRu ?? film.nameEn ?? "Логотип"}
+                  fill
+                  sizes="(max-width: 639px) calc(100vw), (max-width: 767px) calc((100vw - 10px) / 2), (max-width: 1023px) calc((100vw - 350px) / 4), calc((100vw - 370px) / 6)"
+                  unoptimized
+                  className="object-cover"
+                />
+                <div className="absolute top-2 left-2">
+                  {film.ratingKinopoisk && (
+                    <RatingBadge value={film.ratingKinopoisk} />
+                  )}
+                </div>
+              </div>
+            </Card>
+          </Link>
+        );
+      })}
+    </div>
+  );
+}
+
+export default MovieGrid;
