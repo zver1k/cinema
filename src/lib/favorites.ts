109c12fdc96db0d622cb6813cd10e7fe9cfeb348
Damir <mail@damirmedia.ru>
2026-05-29 23:54:41 +0500

    feat(profile): добавить загрузку фильмов избранного и списков
    getFavoriteMovies и getWatchListMovies берут id из БД и параллельно
    догружают детали фильмов через Promise.all

--- src/lib/favorites.ts
+++ src/lib/favorites.ts
@@ -1,6 +1,7 @@
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { prisma } from "@/lib/prisma";
+import { getFilmsById } from "@/shared/api/films";
 
 export async function isFavorite(movieId: string) {
   const session = await auth.api.getSession({ headers: await headers() });
@@ -10,4 +11,17 @@ export async function isFavorite(movieId: string) {
     where: { userId_movieId: { userId, movieId } },
   });
   return !!existing;
+}
+
+export async function getFavoriteMovies() {
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session) return [];
+  const userId = session.user.id;
+  const listFavoriteMovies = await prisma.favorite.findMany({
+    where: { userId },
+  });
+  const list = await Promise.all(
+    listFavoriteMovies.map((fav) => getFilmsById(fav.movieId)),
+  );
+  return list;
 }
--- src/lib/watchlist.ts
+++ src/lib/watchlist.ts
@@ -2,6 +2,7 @@ import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { prisma } from "@/lib/prisma";
 import { WatchStatus } from "@/generated/prisma/enums";
+import { getFilmsById } from "@/shared/api/films";
 
 export async function getWatchStatus(
   movieId: string,
@@ -13,4 +14,17 @@ export async function getWatchStatus(
     where: { userId_movieId: { userId, movieId } },
   });
   return existing?.status ?? null;
+}
+
+export async function getWatchListMovies({ status }: { status: WatchStatus }) {
+  const session = await auth.api.getSession({ headers: await headers() });
+  if (!session) return [];
+  const userId = session.user.id;
+  const listWatchListMovies = await prisma.watchlist.findMany({
+    where: { userId, status },
+  });
+  const list = await Promise.all(
+    listWatchListMovies.map((fav) => getFilmsById(fav.movieId)),
+  );
+  return list;
 }
