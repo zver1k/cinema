1e14e684cad1dca68f4d616b89f7c80035d35891
zver1k <mail@damirmedia.ru>
2026-06-05 20:48:46 +0500

    feat(movies): показать digest как код ошибки в error boundary

--- src/app/(main)/movies/error.tsx
+++ src/app/(main)/movies/error.tsx
@@ -11,13 +11,13 @@ function Error({
   unstable_retry: () => void;
 }) {
   useEffect(() => {
-    // Log the error to an error reporting service
     console.error(error);
   }, [error]);
   return (
     <div className="h-full flex flex-col gap-6 justify-center items-center">
       <h2>Что-то случилось, попробуйте позже!</h2>
       <Button onClick={() => unstable_retry()}>Загрузить снова</Button>
+      <p>{error.digest ? `Код ошибки: ${error.digest}` : ""}</p>
     </div>
   );
 }
